57fef9c4b35d295e08089ed358b60666bf640789
FlyingWalross <jakob.jandl@gmail.com>
2023-06-09 15:40:38 +0200

    added scaling and standardization

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ from sklearn.model_selection import train_test_split
 # get the data from the .csv files (combines weekend and weekday data)
 vienna = acq.get_vienna_data()
 
-# explore the data (plots etc.) and clean it (remove outliers, check class imbalance etc.)
+# explore the data (plots etc.) and clean it
+# (remove outliers, check class imbalance, check encoding, dimensionality reduction of unnecessary columns)
 vienna = exp.explore_and_clean(vienna)
 
-# preprocess the data (normalize, scale, encoding, dimensionality reduction etc.)
+# preprocess the data (normalize, scale)
 vienna = pre.preprocess_data(vienna)
 
 # now we can use the data in the different models
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,3 +1,19 @@
-def preprocess_data(city_data):
-    return city_data
+from sklearn.preprocessing import MinMaxScaler
 
+
+def preprocess_data(city_data):
+    # we use a MinMaxScaler to scale the data to the range [0,1]
+    scaler = MinMaxScaler()
+
+    # scale all columns except the target column 'realSum' and boolean columns
+    columns_to_scale = ['person_capacity', 'cleanliness_rating', 'guest_satisfaction_overall', 'bedrooms', 'dist',
+                        'metro_dist', 'attr_index', 'rest_index', 'lng', 'lat']
+    city_data[columns_to_scale] = scaler.fit_transform(city_data[columns_to_scale])
+
+    # standardize all columns except the target column 'realSum' and boolean columns
+    # we use Z-transform (x - mean) / std
+    columns_to_standardize = ['person_capacity', 'cleanliness_rating', 'guest_satisfaction_overall', 'bedrooms', 'dist',
+                              'metro_dist', 'attr_index', 'rest_index', 'lng', 'lat']
+    # city_data[columns_to_standardize] = (city_data[columns_to_standardize] - city_data[columns_to_standardize].mean()) / city_data[columns_to_standardize].std()
+
+    return city_data
