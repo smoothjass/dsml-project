11f651f1fb6b2c6f9d22242d04298ea43ba171f6
smoothjass <100789381+smoothjass@users.noreply.github.com>
2023-06-10 20:57:55 +0200

    - try different feature combinations

--- main.py
+++ main.py
@@ -23,6 +23,23 @@ def calculateErrors(y_test, y_hat):
     return mae, rmse, medae
 
 
+def fitAndTune(X_train, y_train, X_test, features):
+    # y_hat_tree =
+    # y_hat_knn =
+    # y_hat_lin_regr =
+    # y_hat_nn = nn.fitAndTuneRandomized(X_train, y_train, X_test)
+    y_hat_nn = nn.predict(X_train, y_train, X_test)
+
+    y_hats = [y_test, y_test, y_test, y_hat_nn]
+    models = ['tree', 'knn', 'lin_regr', 'nn']
+    feature_group = [features, features, features, features]
+    dictionary = {'model': models,
+                  'y_hat': y_hats,
+                  'features': feature_group}
+    predictions = pd.DataFrame(dictionary)
+    return predictions
+
+
 # Machine Learning Workflow
 
 # step 1
@@ -43,30 +60,49 @@ vienna = pre.preprocess_data(vienna)
 X = vienna.drop(['realSum'], axis=1)  # .to_numpy()
 y = vienna['realSum']  # .to_numpy()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+# feature selection and splitting
+# spatial features
+X_spatial = X[['lat', 'lng', 'dist', 'metro_dist', 'rest_index', 'attr_index']]
+X_train_spatial, X_test_spatial, y_train_spatial, y_test_spatial = train_test_split(X_spatial, y, random_state=42)
+# quality features
+X_quality = X[['cleanliness_rating', 'guest_satisfaction_overall', 'weekend']]
+X_train_quality, X_test_quality, y_train_quality, y_test_quality = train_test_split(X_quality, y, random_state=42)
+# basic features
+X_basic = X[['host_is_superhost', 'room_private', 'person_capacity', 'bedrooms', 'entire_home_apt']]
+X_train_basic, X_test_basic, y_train_basic, y_test_basic = train_test_split(X_basic, y, random_state=42)
+# combinations of features
+X_spatial_and_basic = pd.concat([X_spatial, X_basic], axis=1)
+X_train_spatial_and_basic, X_test_spatial_and_basic, y_train_spatial_and_basic, y_test_spatial_and_basic = \
+    train_test_split(X_spatial_and_basic, y, random_state=42)
+X_quality_and_basic = pd.concat([X_quality, X_basic], axis=1)
+X_train_quality_and_basic, X_test_quality_and_basic, y_train_quality_and_basic, y_test_quality_and_basic = \
+    train_test_split(X_quality_and_basic, y, random_state=42)
 
 # step 5 fit models using training set + hyperparameter tuning including cross validation with random-/gridsearch
-# todo for each model: a function which takes training set (X and y) and X_test as input, performs fit and
+# todo - for each model: a function which takes training set (X and y) and X_test as input, performs fit and
 #  hyperparameter tuning with cross validation, stores best model for future use and returns y_hat
-print("fitting")
-#y_hat_tree =
-#y_hat_knn =
-#y_hat_lin_regr =
-y_hat_nn = nn.fitAndTune(X_train, y_train, X_test)
-#predictions = [y_hat_tree, y_hat_knn, y_hat_lin_regr, y_hat_nn]
-
-y_hats = [y_test, y_test, y_test, y_hat_nn]
-models = ['tree', 'knn', 'lin_regr', 'nn']
-dictionary = {'model': models,
-              'y_hat': y_hats}
-predictions = pd.DataFrame(dictionary)
+# todo - try each model with different feature combinations
+predictions_all = fitAndTune(X_train, y_train, X_test, 'all')
+predictions_spatial = fitAndTune(X_train_spatial, y_train_spatial, X_test_spatial, 'spatial')
+predictions_quality = fitAndTune(X_train_quality, y_train_quality, X_test_quality, 'quality')
+predictions_basic = fitAndTune(X_train_basic, y_train_basic, X_test_basic, 'basic')
+predictions_spatial_and_basic = fitAndTune(X_train_spatial_and_basic, y_train_spatial_and_basic,
+                                           X_test_spatial_and_basic, 'spatial_and_basic')
+predictions_quality_and_basic = fitAndTune(X_train_quality_and_basic, y_train_quality_and_basic,
+                                           X_test_quality_and_basic, 'quality_and_basic')
+predictions = [predictions_all, predictions_spatial, predictions_quality, predictions_basic,
+                predictions_spatial_and_basic, predictions_quality_and_basic]
 
 # step 6
 # performance evaluation with test set
-performance = []
-for index, row in predictions.iterrows():
-    mae, rmse, medae = calculateErrors(y_test, row['y_hat'])
-    performance.append([row['model'], mae, rmse, medae])
-performance = pd.DataFrame(performance, columns=["model", "mae", "rmse", "medae"])
+evaluations = pd.DataFrame()
+for df in predictions:
+    evaluation = []
+    for index, row in df.iterrows():
+        mae, rmse, medae = calculateErrors(y_test, row['y_hat'])
+        evaluation.append([row['model'], row['features'], mae, rmse, medae])
+    evaluation = pd.DataFrame(evaluation, columns=["model", "features", "mae", "rmse", "medae"])
+    evaluations = pd.concat([evaluations, evaluation], axis=0)
 
 # step 7
 # summarize results
--- model/neuralnetwork.py
+++ model/neuralnetwork.py
@@ -2,6 +2,8 @@
 # IMPORTS
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+
 ########################################################################################################################
 
 ########################################################################################################################
@@ -25,16 +27,18 @@ The parameters of the estimator used to apply these methods are optimized by cro
 settings.
 In contrast to GridSearchCV, not all parameter values are tried out, but rather a fixed number of parameter settings 
 is sampled from the specified distributions. The number of parameter settings that are tried is given by n_iter."""
+
+
 ########################################################################################################################
 
 ########################################################################################################################
 # IMPLEMENTATION
-def fitAndTune(X_train, y_train, X_test):
-    y_hat = None
+def fitAndTuneRandomized(X_train, y_train, X_test):
     # Define the parameter grid for RandomizedSearchCV
     param_grid = {
-        'hidden_layer_sizes': [(1,), (2,), (3,), (5,), (7,), (10,),
-                               (15,), (17,), (19,), (20,), (21,), (23,), (25,)],  # Specify different hidden layer sizes
+        'hidden_layer_sizes': [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,),
+                               (11,), (12,), (13,), (14,), (15,)],
+        # Specify different hidden layer sizes
         'activation': ['relu', 'tanh'],  # Activation functions to try
         'alpha': [0.0001, 0.001, 0.01]  # Regularization parameter
     }
@@ -44,7 +48,7 @@ def fitAndTune(X_train, y_train, X_test):
 
     # Create the RandomizedSearchCV object
     # default 5-fold cross validation with shuffle_False
-    random_search = RandomizedSearchCV(model, param_grid, verbose=3, n_iter=20)
+    random_search = RandomizedSearchCV(model, param_grid, verbose=3, n_iter=50)
 
     # Fit the RandomizedSearchCV object to the training data
     random_search.fit(X_train, y_train)
@@ -59,35 +63,87 @@ def fitAndTune(X_train, y_train, X_test):
     y_hat = best_model.predict(X_test)
 
     return y_hat
+
+
+def fitAndTuneGrid(X_train, y_train, X_test):
+    # Define the parameter grid for GridSearchCV
+    param_grid = {
+        'hidden_layer_sizes': [(1,), (2,), (3,), (4,), (5,), (6,), (7,), (8,), (9,), (10,),
+                               (11,), (12,), (13,), (14,), (15,)],
+        # Specify different hidden layer sizes
+        'activation': ['relu', 'tanh'],  # Activation functions to try
+        'alpha': [0.0001, 0.001, 0.01]  # Regularization parameter
+    }
+    # Create the MLPRegressor model
+    # lbfgs converges faster
+    model = MLPRegressor(random_state=42, max_iter=3000, solver='lbfgs')
+
+    # Create the RandomizedSearchCV object
+    # default 5-fold cross validation with shuffle_False
+    grid_search = GridSearchCV(model, param_grid, verbose=3)
+
+    # Fit the RandomizedSearchCV object to the training data
+    grid_search.fit(X_train, y_train)
+
+    # Print the best hyperparameters found
+    print("Best Hyperparameters: ", grid_search.best_params_)
+    # Best Hyperparameters: {'activation': 'relu', 'alpha': 0.0001, 'hidden_layer_sizes': (13,)}
+
+    # Get the best model found by RandomizedSearchCV
+    best_model = grid_search.best_estimator_
+
+    # Predict y_hat using the best model
+    y_hat = best_model.predict(X_test)
+
+    return y_hat
+
+# once the number 13 was found as the best hidden layer size, we tried tuning the other parameters but the
+# improvement was not significant
+def fitAndTuneActivationAndAlpha(X_train, y_train, X_test):
+    # Define the parameter grid for GridSearchCV
+    param_grid = {
+        'activation': ['relu', 'tanh', 'logistic', 'identity'],  # Activation functions to try
+        'alpha': [0.00001, 0.0001, 0.001, 0.01, 0.1]  # Regularization parameter
+    }
+    # Create the MLPRegressor model
+    # lbfgs converges faster
+    model = MLPRegressor(random_state=42, max_iter=3000, solver='lbfgs', hidden_layer_sizes=(13,))
+
+    # Create the RandomizedSearchCV object
+    # default 5-fold cross validation with shuffle_False
+    grid_search = GridSearchCV(model, param_grid, verbose=3)
+
+    # Fit the RandomizedSearchCV object to the training data
+    grid_search.fit(X_train, y_train)
+
+    # Print the best hyperparameters found
+    print("Best Hyperparameters: ", grid_search.best_params_)
+
+    # Get the best model found by RandomizedSearchCV
+    best_model = grid_search.best_estimator_
+
+    # Predict y_hat using the best model
+    y_hat = best_model.predict(X_test)
+
+    return y_hat
+
+
+def predict(X_train, y_train, X_test):
+    model_all = MLPRegressor(random_state=42, max_iter=3000, solver='lbfgs', hidden_layer_sizes=(13,), activation='relu',
+                         alpha=0.0001)
+
+    # Fit the model to the training data
+    model_all.fit(X_train, y_train)
+
+    # Predict y_hat using the trained model
+    y_hat = model_all.predict(X_test)
+
+    return y_hat
 ########################################################################################################################
 
-# model = MLPRegressor(random_state=42, max_iter=2000)
-# Best Hyperparameters:  {'hidden_layer_sizes': (20,), 'alpha': 0.001, 'activation': 'relu'}
-# nn,51.73184,78.34428,37.00069
-# some did not converge
-# param_grid = {
-#         'hidden_layer_sizes': [(1,), (2,), (3,), (10,), (15,), (20,)],  # Specify different hidden layer sizes
-#         'activation': ['relu', 'tanh'],  # Activation functions to try
-#         'alpha': [0.0001, 0.001, 0.01]  # Regularization parameter
-#     }
-
-# model = MLPRegressor(random_state=42, max_iter=2000)
-# Best Hyperparameters:  {'hidden_layer_sizes': (25,), 'alpha': 0.01, 'activation': 'relu'}
-# nn,51.83142,77.98413,37.25985
-# some did not converge
-# param_grid = {
-#         'hidden_layer_sizes': [(15,), (17,), (19,), (20,),
-#         (21,), (23,), (25,)],  # Specify different hidden layer sizes
-#         'activation': ['relu', 'tanh'],  # Activation functions to try
-#         'alpha': [0.0001, 0.001, 0.01]  # Regularization parameter
-#     }
-
-# model = MLPRegressor(random_state=42, max_iter=2000, solver='lbfgs')
-# Best Hyperparameters:  {'hidden_layer_sizes': (7,), 'alpha': 0.0001, 'activation': 'relu'}
-# nn,51.00826,75.35174,36.07388
-# param_grid = {
-#         'hidden_layer_sizes': [(1,), (2,), (3,), (5,), (7,), (10,), (15,), (17,), (19,),
-#         (20,), (21,), (23,), (25,)],  # Specify different hidden layer sizes
-#         'activation': ['relu', 'tanh'],  # Activation functions to try
-#         'alpha': [0.0001, 0.001, 0.01]  # Regularization parameter
-#     }
+# Best Hyperparameters:  {'hidden_layer_sizes': (13,), 'alpha': 0.0001, 'activation': 'relu'} for all
+# Best Hyperparameters:  {'hidden_layer_sizes': (13,), 'alpha': 0.0001, 'activation': 'relu'} for spatial
+# Best Hyperparameters:  {'hidden_layer_sizes': (7,), 'alpha': 0.0001, 'activation': 'relu'} for quality
+# Best Hyperparameters:  {'hidden_layer_sizes': (14,), 'alpha': 0.0001, 'activation': 'relu'} for basic
+# Best Hyperparameters:  {'hidden_layer_sizes': (12,), 'alpha': 0.01, 'activation': 'relu'} for spatial and basic
+# Best Hyperparameters:  {'hidden_layer_sizes': (10,), 'alpha': 0.001, 'activation': 'relu'} for quality and basic
