a895a2ce5ce1ebbe62c9e8b1df8a309382f7854c
marlis-t <if21b152@technikum-wien.at>
2023-06-12 15:01:42 +0200

    added getBestModel to kNN

--- model/kNN.py
+++ model/kNN.py
@@ -7,9 +7,10 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, roc_auc_score, brier_score_loss
 from sklearn.metrics import mean_absolute_error, mean_squared_error, median_absolute_error
 
-def kNN_tune_hyperparameters(X_train, y_train, X_test, y_test):
+
+def fitAndTuneRandomized(X_train, y_train, X_test):
     # hyperparameters
-    k = list(range(1, 20))  # all options for k that should be tried
+    k = list(range(1, 90))  # all options for k that should be tried
     parameter_grid = {
         'n_neighbors': k,  # Possible values for the number of neighbors
         'weights': ['uniform', 'distance'],
@@ -34,55 +35,39 @@ def kNN_tune_hyperparameters(X_train, y_train, X_test, y_test):
     print("Best params: ", best_params)
     best_regressor = grid_search.best_estimator_
     y_hat = best_regressor.predict(X_test)
-    test_kNN_Regression(y_test, y_hat)
-    return best_regressor
+    return y_hat
 
-def kNN_getBestModel(feature_group):
+
+def getBestModel(feature_group):
     knn_regressor = None
-    '''
+
     if feature_group == "all":
-        knn_regressor = KNeighborsRegressor(n_neighbors= , weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=3, weights='distance', p=1)
         return knn_regressor
     elif feature_group == "spatial":
-        knn_regressor = KNeighborsRegressor(n_neighbors=, weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=88, weights='distance', p=1)
     elif feature_group == "quality":
-        knn_regressor = KNeighborsRegressor(n_neighbors=, weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=39, weights='uniform', p=1)
     elif feature_group == "basic":
-        knn_regressor = KNeighborsRegressor(n_neighbors=, weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=45, weights='distance', p=1)
     elif feature_group == "spatial_and_basic":
-        knn_regressor = KNeighborsRegressor(n_neighbors=, weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=36, weights='distance', p=1)
     elif feature_group == "quality_and_basic":
-        knn_regressor = KNeighborsRegressor(n_neighbors=, weights=, p=)
+        knn_regressor = KNeighborsRegressor(n_neighbors=38, weights='uniform', p=1)
     else:
         print("Unknown feature group")
-    '''
+
     return knn_regressor
 
 
+'''
 def test_kNN_Regression(y_test, y_hat):
-
- mse = mean_squared_error(y_test, y_hat)
- mae = mean_absolute_error(y_test, y_hat)
- rmse = math.sqrt(mean_squared_error(y_test, y_hat))
- medae = median_absolute_error(y_test, y_hat)
- print("Mean Squared Error:", mse)
- print("Mean Absolute Error:", mae)
- print("Root Mean Squared Error:", rmse)
- print("Median Absolute Error:", medae)
-
-
-
+    mse = mean_squared_error(y_test, y_hat)
+    mae = mean_absolute_error(y_test, y_hat)
+    rmse = math.sqrt(mean_squared_error(y_test, y_hat))
+    medae = median_absolute_error(y_test, y_hat)
+    print("Mean Squared Error:", mse)
+    print("Mean Absolute Error:", mae)
+    print("Root Mean Squared Error:", rmse)
+    print("Median Absolute Error:", medae)
 '''
- use like this
-X = vienna.drop('realSum', axis=1)
-
-y = vienna['realSum']
-
-# split into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
-
-y_pred = kNN.kNN_Regression(X_train, y_train, X_test)
-kNN.test_kNN_Regression(y_test, y_pred)
-'''
-
-
